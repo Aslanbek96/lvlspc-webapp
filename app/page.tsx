b6b1bc090d2329df7c7ec4686d216f9402c2d960
Aslanbek96 <gabdullinasl@yandex.kz>
2024-03-26 17:27:29 +0500

    grammy web app

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,36 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { triggerAsyncId } from "async_hooks";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { WebApp } from "@grammyjs/web-app";
 import { useEffect } from "react";
 
 export default function Home() {
   useEffect(() => {
-    const tg = window.Telegram.WebApp;
-    tg.MainButton.isVisible = true;
-    tg.MainButton.hide();
-    tg.MainButton.text = "Отправить данные";
-    tg.headerColor = "secondary_bg_color";
-    tg.ready();
+    WebApp.ready();
   });
   const handleClose = () => {
-    const tg = window.Telegram.WebApp;
-    tg.close();
+    WebApp.close();
   };
 
+  // const tg = window.Telegram.WebApp;
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button onClick={handleClose}>Сохранить</Button>
+    <main className="flex min-h-screen flex-col items-center gap-2 p-24">
+      <div className="w-[240px] flex flex-col gap-4 items-center">
+        <div className="space-y-1 w-full">
+          <Label>Ваше прежнее имя</Label>
+          <Input className="w-full" />
+        </div>
+        <div className="space-y-1 w-full">
+          <Label>Ваше новое имя</Label>
+          <Input className="w-full" />
+        </div>
+        <Button className="w-full" onClick={handleClose}>
+          Сохранить
+        </Button>
+      </div>
     </main>
   );
 }
