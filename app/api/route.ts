0db798324134c6e4dcce92933dc52910c12c0eda
Aslanbek96 <gabdullinasl@yandex.kz>
2024-03-27 12:52:08 +0500

    "own server"

---
+++ app/api/route.ts
@@ -0,0 +1,32 @@
+import { Bot } from "grammy";
+import { NextRequest, NextResponse } from "next/server";
+
+const bot = new Bot(process.env.BOT_TOKEN!);
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const { firstName, lastName, queryId } = body;
+  try {
+    await bot.api.answerWebAppQuery(queryId, {
+      id: queryId,
+      type: "article",
+      title: "Successfull",
+      input_message_content: {
+        message_text: `Hello, ${firstName} ${lastName}`,
+      },
+    });
+
+    return NextResponse.json({ success: true });
+  } catch (error) {
+    await bot.api.answerWebAppQuery(queryId, {
+      id: queryId,
+      type: "article",
+      title: "Unsuccessfull",
+      input_message_content: {
+        message_text: `Hello, ${firstName} ${lastName}`,
+      },
+    });
+
+    return NextResponse.json({ success: false });
+  }
+}
